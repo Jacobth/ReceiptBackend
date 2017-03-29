f20a86d0fab05518669337c2b92e1fcdd36e3cf7
Jacobth <jacob.thureson@gmail.com>
2017-03-29 22:33:06 +0200

    intit

---
+++ server.js
@@ -0,0 +1,125 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+var bodyParser = require('body-parser');
+var StringDecoder = require('string_decoder').StringDecoder;
+var port = 3000;
+var ip = app.adress;
+var async = require('async'); 
+var exec = require('child_process').exec, child;
+
+app.listen(port, function () {
+  console.log('Listening on port ' + port);
+});
+
+function rawBody(req, res, next) {
+    var chunks = [];
+
+    req.on('data', function(chunk) {
+        chunks.push(chunk);
+    });
+
+    req.on('end', function() {
+        var buffer = Buffer.concat(chunks);
+
+        req.bodyLength = buffer.length;
+        req.rawBody = buffer;
+        next();
+    });
+
+    req.on('error', function (err) {
+        console.log(err);
+        res.status(500);
+    });
+}
+
+app.post('/upload-video', rawBody, function (req, res) {
+
+    if (req.rawBody && req.bodyLength > 0) {
+
+        var decoder = new StringDecoder('utf8');
+        var str = decoder.write(req.rawBody);
+
+        var crypto = require('crypto');
+        var link = crypto.createHash('md5').update(str).digest('hex');
+        console.log(link);
+
+        var base = 'C:/Users/jacobth/Documents/ReceiptBackend';
+
+        var videoFile = ".mp4";
+        var newPath = base + "/uploads/video/" + link + videoFile;
+
+        console.log(newPath);
+    
+    async.waterfall([
+        function writeFile(writeFileCallback) {
+            
+            fs.writeFile(newPath, req.rawBody, function (error) {
+                  if (!error) {
+                      console.log("File successfully written");
+                  }
+
+                  writeFileCallback(error);
+              });
+        },
+
+        function processFile(processFileCallback) {
+            child = exec('java -cp ' +  base + '/kvittoscanner_main.jar Main ' + newPath,
+              
+              function (error, stdout, stderr){
+      
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+    
+                if(!error){
+                  console.log('exec error: ' + error);
+                }
+            
+                processFileCallback(null);
+              });
+        }
+
+      ], function (error) {
+          if (error) {
+          }
+    });
+       
+        var imgFile = ".png";
+    var sendPath = __dirname + "/results/test"  + imgFile;
+    
+    //res.sendFile(sendPath);
+
+    //fs.unlink(sendPath);
+    //fs.unlink(newPath);         
+  }
+});
+
+app.post('/upload-image', rawBody, function (req, res) {
+
+    if (req.rawBody && req.bodyLength > 0) {
+
+        var decoder = new StringDecoder('utf8');
+        var str = decoder.write(req.rawBody);
+
+        var crypto = require('crypto');
+        var link = crypto.createHash('md5').update(str).digest('hex');
+        console.log(link);
+
+        var imageFile = ".png";
+        var newPath = __dirname + "/uploads/hdr/" + link + imageFile;
+
+      fs.writeFile(newPath, req.rawBody, function (err) {
+        });
+
+        var child = require('child_process').spawn('java', ['-jar', 'kvittoscanner.jar', newPath]);
+
+        var imgFile = ".png";
+    var sendPath = __dirname + "/results/test"  + imgFile;
+    res.sendFile(sendPath); 
+
+    fs.unlink(sendPath);
+    fs.unlink(newPath);
+  }
+});
